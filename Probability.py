6d921a70d71e978e409e0b16c1478578f3199774
yunost <yunost@yunost.localhost>
2022-02-11 20:00:22 +0300

    Finish probability

--- Probability.py
+++ Probability.py
@@ -1,4 +1,6 @@
 import enum, random
+import math
+from matplotlib import pyplot as plt
 
 
 # Enum - типизированное множество перечислимых значений.
@@ -45,4 +47,33 @@ def uniform_cdf(x: float) -> float:
         return x # Например, P(X <= 0.4) = 0.4
     else:
         return 1 # Равномерная величина всегда меньше 1
+
+SQRT_TWO_PI = math.sqrt(2 * math.pi)
+
+def normal_pdf(x: float, mu: float = 0, sigma: float = 1) -> float:
+    return (math.exp(-(x-mu) ** 2 / 2 / sigma ** 2) / (SQRT_TWO_PI * sigma))
+
+xs = [x / 10.0 for x in range(-50, 50)]
+#plt.plot(xs, [normal_pdf(x, sigma=1) for x in xs], '-', label='мю=0, сигма=1')
+#plt.plot(xs, [normal_pdf(x, sigma=2) for x in xs], '--', label='мю=0, сигма=2')
+#plt.plot(xs, [normal_pdf(x, sigma=0.5) for x in xs], ':', label='мю=0, сигма=0.5')
+#plt.plot(xs, [normal_pdf(x, mu=-1) for x in xs], '-.', label='мю=-1, сигма=1')
+#plt.legend()
+#plt.title("Различные нормальные функции плотности вероятности")
+#plt.show()
+
+
+def normal_cdf(x: float, mu: float = 0, sigma: float = 1) -> float:
+    return (1 + math.erf((x - mu) / math.sqrt(2) / sigma)) / 2
+
+xs = [x / 10.0 for x in range(-50, 50)]
+plt.plot(xs, [normal_cdf(x, sigma=1) for x in xs], '-', label='мю=0, сигма=1')
+plt.plot(xs, [normal_cdf(x, sigma=2) for x in xs], '--', label='мю=0, сигма=2')
+plt.plot(xs, [normal_cdf(x, sigma=0.5) for x in xs], ':', label='мю=0, сигма=0.5')
+plt.plot(xs, [normal_cdf(x, mu=-1) for x in xs], '-.', label='мю=-1, сигма=1')
+plt.legend(loc=4) # Внизу справа
+plt.title("Различные нормальные кумулятивные функции распределения")
+plt.show()
+
+
 
